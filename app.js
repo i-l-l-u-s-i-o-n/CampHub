7dd082cdb77fb2897e51fac14ab35a25ccef6cb5
Shivam Shukla <shivamshukla2297@gmail.com>
2019-03-30 11:28:15 +0000

    Added Mongo DB to store campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-var express = require("express");
-var app=express();
-var bodyParser=require("body-parser");
+// Including the required packages.
+var express     = require("express"),
+    app         =express(),
+    bodyParser  =require("body-parser"),
+    mongoose    = require("mongoose");
+    
+    
+    
+//Using body parser to read data from the parameter send using POST requests.
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Doing this will consider all the file to be rendered as EJS(embedded JS)
+app.set("view engine","ejs");
+
+
+
+// Connecting to Mongo DB using mongoose.
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true })
+
+
+// Schema SETUP
+
+var campgroundSchema =new mongoose.Schema({
+    name:String,
+    image:String
+});
+
+
+// Compiling schema into MODEL to use various methods.
+var campground=mongoose.model("Campground",campgroundSchema);
+
+
+
+
+
 
 var campgrounds=[
     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
@@ -12,14 +44,13 @@ var campgrounds=[
         {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
         {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
         {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}    ]
-
-//Using body parser to read data from the parameter send using POST requests.
-app.use(bodyParser.urlencoded({extended:true}))
-app.set("view engine","ejs");
-
+        
+        
 app.get("/",function(req,res){
     res.render("home");
 });
+
+
 
 
 
